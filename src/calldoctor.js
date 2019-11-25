59e72449964eea91c33fe861ed15b345492e11eb
Peter Wells <petethebeatwells@gmail.com>
2019-11-24 23:21:23 -0800

    allow for searches by condition as well as doctor name and return results

--- src/calldoctor.js
+++ src/calldoctor.js
@@ -1,5 +1,4 @@
 // This is the API Call for BetterDoctor
-
 export class DoctorService {
   constructor (query, location){
     this.query = query;
@@ -20,31 +19,23 @@ export class DoctorService {
         console.error("You've caused an error. " + error.message);
         return error;
       }
+    }
+    async callDoctorByCondition(){
+      try {
+        let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${this.query}&location=${this.location}&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+          if (response.ok === false){
+            throw new Error(`Code: ${response.status}: ${response.statusText}`);
+          }
+          else {
+            let jsonifiedResponse = await response.json();
+            console.log(jsonifiedResponse);
+            return jsonifiedResponse;
+          }
+        } catch (error) {
+          console.error("You've caused an error. " + error.message);
+          return error;
+        }
+      }
+    }
 
-
-      // async callDoctorByCondition(){
-        //   try{
-          //     let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${this.query}&location=${this.location}&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-            //       if (response.ok) {
-              //         let jsonifiedResponse = await response.json();
-              //         return jsonifiedResponse;
-              //       } else {
-                //         throw new Error('something went wrong');
-                //       }
-                //     } catch (error) {
-                  //       console.error("You've caused an error. Error Text: " + error.message);
-                  //     }
-                  //   }
-
-                  // Found code sample to throw errors here: https://stackoverflow.com/questions/38235715/fetch-reject-promise-and-catch-the-error-if-status-is-not-ok
-
-                  // async callDoctorByCondition(){
-
-                    // }
-                  }
-                }
-
-
-
-
-                // let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${this.query}&location=${this.location}&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+    // Found code sample to throw errors here: https://stackoverflow.com/questions/38235715/fetch-reject-promise-and-catch-the-error-if-status-is-not-ok
